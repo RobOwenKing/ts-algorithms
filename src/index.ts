2b0cf28d57c2c9ffeff2620f903b880e3f1af88b
Robert King <robert.o.king@hotmail.co.uk>
2022-09-18 16:50:19 +0100

    feat: lookAndSee() to return the next term in the sequence for a given num

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-function component(message: string) {
-  const element = document.createElement("div");
+const lookAndSee = (num: string): string => {
+  let currentChar = num[0];
+  let count = 0;
+  let newNum = "";
 
-  element.innerHTML = message;
+  for (const char of num + ".") {
+    if (char === currentChar) {
+      count += 1;
+    } else {
+      newNum += count;
+      newNum += currentChar;
+      count = 1;
+      currentChar = char;
+    }
+  }
+
+  return newNum;
+};
+
+function app() {
+  const element = document.createElement("div");
 
   return element;
 }
 
-document.getElementById('app').appendChild(component("Hello from Typescript with live reloading?"));
+document.getElementById("app").appendChild(app());
