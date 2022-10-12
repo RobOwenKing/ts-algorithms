f11128cc5c6a09fed26e7dcdef259b11dc1cb8c6
Robert King <robert.o.king@hotmail.co.uk>
2022-10-12 13:57:41 +0100

    feat: Stack data structure

---
+++ src/data_structures/stack.ts
@@ -0,0 +1,19 @@
+export class Stack<Type> {
+  private stack: Type[];
+
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element: Type): void {
+    this.stack.push(element);
+  }
+
+  pop(): Type | undefined {
+    return this.stack.pop();
+  }
+
+  peek(): Type | undefined {
+    return this.stack.at(-1);
+  }
+}
