11d842a810e4ac7b7f2fb4fd8e7de794b18f1eb2
Robert King <robert.o.king@hotmail.co.uk>
2022-10-12 18:58:32 +0100

    feat: update() function for Balanced Parentheses

--- src/algorithms/balancedParentheses.ts
+++ src/algorithms/balancedParentheses.ts
@@ -3,20 +3,24 @@ import { Page } from "../types";
 import { Stack } from "../data_structures/stack";
 
 const areParenthesesBalanced = (str: string): boolean => {
-  return true;
+  return str.length < 5;
 };
 
 export const balancedParenthesesPage: Page = {
   name: "Balanced Parentheses",
   markup: [
     ["h2", {}, "Balanced Parentheses"],
-    ["p", {}, "text."],
-    ["textarea", {}],
-    ["h3", {}, "Output"],
+    ["p", {}, "Checks valid bracketing of the pairs: (), [], {} and <>."],
+    ["textarea", { id: "input" }],
+    ["h3", {}, "Are the Parentheses Balanced?"],
     ["div", { id: "output" }],
   ],
-  inputs: ["seed", "terms"],
+  inputs: ["input"],
   update: () => {
-    areParenthesesBalanced("temp");
+    const input = <HTMLTextAreaElement>document.getElementById("input");
+    const output = document.getElementById("output");
+    const isBalanced = areParenthesesBalanced(input.value);
+
+    output.innerText = isBalanced ? "Yes" : "No";
   },
 };
