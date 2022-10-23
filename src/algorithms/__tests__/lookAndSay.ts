bef560169b62eb6483d8a3ce9d1d8a088850b0e9
Robert King <robert.o.king@hotmail.co.uk>
2022-10-24 00:37:35 +0100

    test: Complex input to getNextLookAndSayTerm()

--- src/algorithms/__tests__/lookAndSay.ts
+++ src/algorithms/__tests__/lookAndSay.ts
@@ -1,9 +1,21 @@
-import { expect, test } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 
 import { getNextLookAndSayTerm } from "../lookAndSay";
 
-test("works with single digits", () => {
-  expect(getNextLookAndSayTerm("0")).toBe("10");
-  expect(getNextLookAndSayTerm("1")).toBe("11");
-  expect(getNextLookAndSayTerm("4")).toBe("14");
+describe("getNextLookAndSayTerm()", () => {
+  test("works with single digits", () => {
+    expect(getNextLookAndSayTerm("0")).toBe("10");
+    expect(getNextLookAndSayTerm("1")).toBe("11");
+    expect(getNextLookAndSayTerm("4")).toBe("14");
+  });
+  test("works with simple strings of digits", () => {
+    expect(getNextLookAndSayTerm("01")).toBe("1011");
+    expect(getNextLookAndSayTerm("10")).toBe("1110");
+    expect(getNextLookAndSayTerm("234")).toBe("121314");
+  });
+  test("works with complex strings of digits", () => {
+    expect(getNextLookAndSayTerm("00")).toBe("20");
+    expect(getNextLookAndSayTerm("22")).toBe("22");
+    expect(getNextLookAndSayTerm("0001000")).toBe("301130");
+  });
 });
